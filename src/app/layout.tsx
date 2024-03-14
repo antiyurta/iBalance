eb3dd36a9429c249245962d0e0891da52dcc413b
gantuya467 <gantuya467@gmail.com>
2024-03-14 19:31:46 +0800

    layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
                   },
                   Segmented: {
                     itemSelectedBg: "#198754",
-                    itemSelectedColor: "white",
+                    itemSelectedColor: "#fff",
                   }
                 },
                 algorithm: theme.compactAlgorithm,
